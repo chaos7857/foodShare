c424f6162fa1c6cdab1c074373b68948118c9e7b
cc_hw <173192386@qq.com>
2025-06-19 21:47:17 +0800

    引入knife4j接口文档，删除自带接口，添加第一个接口，健康检测

---
+++ src/main/java/com/cc/backend/controller/CommonController.java
@@ -0,0 +1,13 @@
+package com.cc.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "OK";
+    }
+}
